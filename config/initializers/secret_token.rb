a9804eaaab65b59516f3175c99e78fcfbc37ecda
Ryan Moret <ryancmoret@gmail.com>
2012-12-02 19:26:31 -0600

    first commit; new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dpthomas::Application.config.secret_token = '6fc3d54db6068993563ae585b1dc93ae36b93862607c54991384f995a8e7dbc16b960bda1ebec80c82c5508b0c48ec1d6f617cce7aa639a99b9f809808230dec'
