32f44efcf240a7b56f77de7b232d27ee6f99a540
Ryan Moret iMac <ryancmoret@gmail.com>
2013-05-18 08:29:08 -0500

    removed old models

--- app/models/email_list.rb
+++
@@ -1,3 +0,0 @@
-class EmailList < ActiveRecord::Base
-  attr_accessible :email_address, :first_name, :last_name
-end
--- app/models/home.rb
+++
@@ -1,3 +0,0 @@
-class Home < ActiveRecord::Base
-  attr_accessible :content, :title
-end
