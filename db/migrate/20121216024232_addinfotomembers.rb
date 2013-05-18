6f2c7e3b22dca9ca6420a3f4c08e3663ef962c70
Ryan Moret iMac <ryancmoret@gmail.com>
2013-05-18 07:59:53 -0500

    removed old migrations

--- db/migrate/20121216024232_addinfotomembers.rb
+++
@@ -1,8 +0,0 @@
-class Addinfotomembers < ActiveRecord::Migration
-  def change
-    add_column :members, :name, :string
-    add_column :members, :profile, :text
-    add_column :members, :title, :string
-  end
-
-end
