eeb7851dc1bcc84ae6e352264c760ff58ff80fd2
Ryan Moret iMac <ryancmoret@gmail.com>
2013-05-18 08:24:14 -0500

    reset controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,7 @@ Dpthomas::Application.routes.draw do
   devise_for :members
 
   root :to => 'home#index'
-  get 'about_us' =>'members#index', :as => 'about_us'
-  get 'email' => 'email_list#new', :as => 'email'
-  post 'email' => 'email_list#create', :as => 'email'
 
-  resources :email_list
-  resources :events, only: [:index]
-  resources :donations, only: [:index]
-  resources :members
-  resources :services, only: [:index]
+  resources :pages, only: [:update, :show]
 
 end
