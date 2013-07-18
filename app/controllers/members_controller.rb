96a6a0e7fc423ea17b5ef67d5c21f55c8eef2db0
Ryan Moret iMac <ryancmoret@gmail.com>
2013-07-18 18:56:09 -0500

    copy changes

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,12 +2,13 @@ class MembersController < ApplicationController
   expose(:members_with_profiles){ Member.completed_profiles }
   expose(:members)
   expose(:member)
+  expose(:future_events){ Event.in_the_future }
 
   before_filter :authenticate!, except: [:index, :show]
 
   def update
     if member.save
-      redirect_to members_dashboard_path, notice: 'You successfully updated your profile'
+      redirect_to members_path, notice: 'You successfully updated your profile'
     else
       render :edit, alert: 'There was an error updating your profile'
     end
