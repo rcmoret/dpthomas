153da1b87584beb46428988252c5f7aaddb01e21
Ryan Moret iMac <ryancmoret@gmail.com>
2013-08-08 02:07:15 -0500

    most responsive design

--- app/assets/javascripts/additional.js
+++ app/assets/javascripts/additional.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
         $('#reply_form').slideToggle();
     });
     $('#flash').hide(3000)
+    windowWidth = window.innerWidth
+    if (windowWidth < 786){
+        $('#member-actions h3').click(function() {
+            $('#action-items').slideToggle();
+        });
+    }
 });
