43e021f2b6332118ece8bb11103619a8ab3a9b86
Ryan Moret iMac <ryancmoret@gmail.com>
2013-08-08 21:39:17 -0500

    h2 tags display paged title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  expose(:page_title)
+
+  def page_title
+    controller = params[:controller]
+    action = params[:action]
+    return controller.capitalize if ['services', 'events'].include?(controller)
+    return 'Donate' if action == 'donate'
+    return 'Home Page' if controller == 'home' && action == 'index'
+    if controller == 'members'
+      return 'Foundation Information' if current_member
+      return 'About the Foundation'
+    end
+  end
 
   def after_sign_in_path_for(member)
     if current_member.incomplete_profile?
