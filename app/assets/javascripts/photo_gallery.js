acf66e6a0f98e33eae69bca1ec2d43876063571b
Ryan Moret iMac <ryancmoret@gmail.com>
2013-08-16 12:47:37 -0500

    images now rotating, but not sliding

--- app/assets/javascripts/photo_gallery.js
+++ app/assets/javascripts/photo_gallery.js
@@ -3,10 +3,30 @@ function setHeight() {
     $('#photo-gallery').css('height', divWidth + 'px');
 }
 
+function slider() {
+    var imageCount = $('.photos img').length - 1;
+    var index = 0
+    setInterval( function() {
+        image = $('.photos img')[index]
+        $(image).removeClass('active')
+        if (index === imageCount) {
+            index = 0
+        } else {
+            index += 1
+        }
+        image = $('.photos img')[index]
+        $(image).addClass('active')
+    }, 3000)
+}
+
 $(document).ready(function() {
     setHeight();
     $('.photos img:first-of-type').addClass('active')
 })
+
+window.onload = function start() {
+    slider();
+}
 
 window.onresize = function(ev) {
     setHeight()
