6118956a7fc42221cdac3e0a6e8136a288944fc2
Ryan Moret <rmoret@changehealthcare.com>
2014-06-25 09:34:35 -0500

    image tags for 2013 walk/run

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @image_files = Dir["#{Rails.root}/app/assets/images/2013_walk_run/*.jpg"].collect do |f|
+      f[/2013_walk_run.*$/]
+    end
   end
 
   def show
