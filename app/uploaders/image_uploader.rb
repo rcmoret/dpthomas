a7a2cebe550ca874be5baf046e19802d251ba4a1
Ryan Moret <rmoret@changehealthcare.com>
2014-06-25 18:07:24 -0500

    modified uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,14 +13,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
 
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    if Rails.env == 'development'
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    else
+      "#{Rails.root}/public/uploads/member/image/"
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
